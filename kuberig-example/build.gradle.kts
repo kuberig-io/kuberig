c10b39d3d593eff623cf4c8617adec384280c34a
Tom Eyckmans <teyckmans@gmail.com>
2019-05-11 13:43:03 +0200

    api-server authentication
    - added some information on how to create the service account in the example build file.

--- kuberig-example/build.gradle.kts
+++ kuberig-example/build.gradle.kts
@@ -18,6 +18,21 @@ kuberig {
         }
         
         create("dev") {
+            /*
+            kubectl create sa kuberig-deployer --namespace=default
+            kubectl create rolebinding kuberig-deployer-edit --clusterrole=edit --serviceaccount=default:kuberig-deployer --namespace=default
+
+            kubectl describe sa kuberig-deployer --namespace=default
+            kubectl describe secret <name-of-token-secret>
+
+            copy the token in environments/<environment-name>/.plain.<environment-name.access-token
+
+            run encrypt<Environment-name>Environment
+
+            you are ready to run deploy<Environment-name>Environment
+
+            update the api server url.
+             */
             apiServer = "https://58788ad0-0a17-4f15-a76f-c0406bcd7e40.k8s.ondigitalocean.com"
         }
     }
