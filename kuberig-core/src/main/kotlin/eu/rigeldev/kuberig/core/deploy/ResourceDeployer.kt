4484502a2112438c2329e31032f01ec3d04c8756
Tom Eyckmans <teyckmans@gmail.com>
2019-07-02 22:19:41 +0200

    make the ResourceDeployer deal with a PUT 422 reply from the api-server

--- kuberig-core/src/main/kotlin/eu/rigeldev/kuberig/core/deploy/ResourceDeployer.kt
+++ kuberig-core/src/main/kotlin/eu/rigeldev/kuberig/core/deploy/ResourceDeployer.kt
@@ -1,8 +1,10 @@
 package eu.rigeldev.kuberig.core.deploy
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.PathNotFoundException
+import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.Unirest
 import com.mashape.unirest.request.HttpRequest
 import eu.rigeldev.kuberig.config.KubeRigEnvironment
@@ -17,6 +19,8 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.ssl.SSLContexts
+import org.json.JSONObject
+import org.json.JSONTokener
 import java.io.File
 
 class ResourceDeployer(private val projectDirectory: File,
@@ -122,15 +126,14 @@ class ResourceDeployer(private val projectDirectory: File,
     }
 
     private fun deploy(methodResult: ResourceGeneratorMethodResult): ResourceGeneratorMethodResult {
-
-
+        println("------")
 
         if (methodResult is SuccessResult) {
-            val json = objectMapper.valueToTree<JsonNode>(methodResult.resource)
+            val newJson = JSONObject(JSONTokener(objectMapper.writeValueAsString(methodResult.resource)))
 
-            val apiVersion = json.get("apiVersion").textValue().toLowerCase()
-            val kind = json.get("kind").textValue()
-            val resourceName = json.get("metadata").get("name").textValue()
+            val apiVersion = newJson.getString("apiVersion").toLowerCase()
+            val kind = newJson.getString("kind")
+            val resourceName = newJson.getJSONObject("metadata").getString("name")
 
             val apiOrApisPart = if (apiVersion == "v1") {
                 "api"
@@ -145,8 +148,8 @@ class ResourceDeployer(private val projectDirectory: File,
 
             val apiResource = apiResourceList.body.resources.first{ it.kind == kind }
 
-            val namespace = if (json.get("metadata").hasNonNull("namespace")) {
-                json.get("metadata").get("namespace").textValue()
+            val namespace = if (newJson.getJSONObject("metadata").has("namespace")) {
+                newJson.getJSONObject("metadata").getString("namespace")
             } else {
                 "default"
             }
@@ -163,7 +166,7 @@ class ResourceDeployer(private val projectDirectory: File,
                 this.addAuthentication(post)
                 val postResponse = post
                         .header("Content-Type", "application/json")
-                        .body(json)
+                        .body(newJson)
                         .asString()
 
                 if (postResponse.status != 201) {
@@ -177,17 +180,122 @@ class ResourceDeployer(private val projectDirectory: File,
             } else {
                 val put = Unirest.put("$targetUrl/$resourceName")
                 this.addAuthentication(put)
+
+                val currentObject = getResult.body.`object`
+
+                val updateJson = JSONObject(newJson.toString())
+
+                updateJson.getJSONObject("metadata").put("resourceVersion", currentObject.getJSONObject("metadata").getString("resourceVersion"))
+
                 val putResponse = put
                         .header("Content-Type", "application/json")
-                        .body(json)
+                        .body(updateJson)
                         .asString()
 
-                if (putResponse.status != 200) {
-                    println(putResponse.status)
-                    println(putResponse.statusText)
-                    println(putResponse.body)
-                    System.out.println("Failed to update $kind from resource generator method ${methodResult.method.generatorType} - ${methodResult.method.methodName}")
-                } else {
+                if (putResponse.status == 422) {
+                    val currentJsonPathContext = JsonPath.parse(currentObject.toString())
+                    val newJsonPathContext = JsonPath.parse(updateJson.toString())
+
+                    val statusObject = JSONObject(putResponse.body)
+                    val statusReason = statusObject.getString("reason")
+
+                    var newJsonUpdated = false
+                    var recreateNeeded = false
+
+                    if (statusReason.toLowerCase() == "invalid") {
+
+                        val detailsObject = statusObject.getJSONObject("details")
+                        val causesArray = detailsObject.getJSONArray("causes")
+
+                        var causesArrayIndex = 0
+                        while (!recreateNeeded && causesArrayIndex < causesArray.length()) {
+                            val causeObject = causesArray.getJSONObject(causesArrayIndex)
+
+                            val causeReason = causeObject.getString("reason")
+                            val causeMessage = causeObject.getString("message")
+                            val causeField = causeObject.getString("field")
+
+                            if (causeReason == "FieldValueInvalid" && causeMessage.endsWith("field is immutable")) {
+                                // copy from original if the new object does not specify it
+                                val fieldJsonPath = "$.$causeField"
+                                val currentFieldValue = currentJsonPathContext.read<Any>(fieldJsonPath)
+
+                                val newFieldValue = try {
+                                    newJsonPathContext.read<Any>(fieldJsonPath)
+                                } catch (e: PathNotFoundException) {
+                                    null
+                                }
+
+                                if (newFieldValue == null) {
+                                    val lastDotIndex = fieldJsonPath.lastIndexOf('.')
+                                    val pathPart = fieldJsonPath.substring(0, lastDotIndex)
+                                    val keyPart = fieldJsonPath.substring(lastDotIndex + 1)
+
+                                    newJsonPathContext.put(pathPart, keyPart, currentFieldValue)
+
+                                    newJsonUpdated = true
+                                } else {
+                                    // new resource definition - requires update of immutable field
+                                    recreateNeeded = true
+                                }
+                            }
+
+                            causesArrayIndex++
+                        }
+
+                    }
+
+                    if (newJsonUpdated){
+
+                        val retryPut = Unirest.put("$targetUrl/$resourceName")
+                        this.addAuthentication(retryPut)
+
+                        val retryJson = newJsonPathContext.jsonString()
+
+                        val putRetryResponse = retryPut
+                                .header("Content-Type", "application/json")
+                                .body(retryJson)
+                                .asString()
+
+                        if (putRetryResponse.status != 200) {
+                            this.putFailure(putRetryResponse, kind, methodResult)
+                        } else {
+                            println("updated $kind - $resourceName in $namespace namespace")
+                        }
+
+                    } else if (recreateNeeded) {
+
+                        val delete = Unirest.delete("$targetUrl/$resourceName")
+                        this.addAuthentication(delete)
+
+                        val deleteResponse = delete.header("Content-Type", "application/json")
+                                .asString()
+
+                        if (deleteResponse.status == 200) {
+                            val recreatePost = Unirest.post(targetUrl)
+                            this.addAuthentication(recreatePost)
+                            val recreatePostResponse = recreatePost
+                                    .header("Content-Type", "application/json")
+                                    .body(newJson)
+                                    .asString()
+
+                            if (recreatePostResponse.status != 201) {
+                                println(recreatePostResponse.status)
+                                println(recreatePostResponse.statusText)
+                                println(recreatePostResponse.body)
+                                println("Failed to create $kind from resource generator method ${methodResult.method.generatorType} - ${methodResult.method.methodName}")
+                            } else {
+                                println("re-created $kind - $resourceName in $namespace namespace")
+                            }
+                        }
+
+                    } else {
+                        this.putFailure(putResponse, kind, methodResult)
+                    }
+
+                } else if (putResponse.status != 200) {
+                    this.putFailure(putResponse, kind, methodResult)
+                }  else {
                     println("updated $kind - $resourceName in $namespace namespace")
                 }
             }
@@ -195,6 +303,14 @@ class ResourceDeployer(private val projectDirectory: File,
 
         return methodResult
     }
+
+    private fun putFailure(putResponse: HttpResponse<String>, kind: String?, methodResult: ResourceGeneratorMethodResult) {
+        println(putResponse.status)
+        println(putResponse.statusText)
+        println(putResponse.body)
+        println("Failed to update $kind from resource generator method ${methodResult.method.generatorType} - ${methodResult.method.methodName}")
+    }
+
 
     private fun addAuthentication(request: HttpRequest) {
         val environmentDirectory = File(projectDirectory, "environments/${this.environment.name}")
