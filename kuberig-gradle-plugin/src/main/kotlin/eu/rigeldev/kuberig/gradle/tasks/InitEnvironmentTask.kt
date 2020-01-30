7e69a2babcc29df261c719f1b37bd7f552ced776
Tom Eyckmans <teyckmans@gmail.com>
2020-01-30 19:41:17 +0100

    #19 - oc command line kube config file support
    - check if there is certificate-authority-data before reading it, default to empty string in case not available. Only write out not empty certificate data to the CA trust file.
    - use $HOME environment variable instead of user.home system property to get the default .kube/config file location.
    - add a branch to use the user/token as AccessTokenUserDetail info.

--- kuberig-gradle-plugin/src/main/kotlin/eu/rigeldev/kuberig/gradle/tasks/InitEnvironmentTask.kt
+++ kuberig-gradle-plugin/src/main/kotlin/eu/rigeldev/kuberig/gradle/tasks/InitEnvironmentTask.kt
@@ -55,9 +55,8 @@ abstract class InitEnvironmentTask: AbstractKubeRigTask() {
         if (currentKubectlContext) {
 
             val kubectlConfigReader = KubectlConfigReader()
-            val contextResult = kubectlConfigReader.readKubectlConfig()
 
-            when (contextResult) {
+            when (val contextResult = kubectlConfigReader.readKubectlConfig()) {
                 is OkContextResult -> {
                     val serviceAccountCreator = ServiceAccountCreator(kubeRigExtension().flags)
 
