2a8d8ba9dcf96b73b30c1be2520a05fe651377e4
Tom Eyckmans <teyckmans@gmail.com>
2020-03-02 21:15:09 +0100

    #21 resource groups
    - support @EnvResources annotation in GroupResourceGenerationFilter.kt

--- kuberig-core/src/main/kotlin/eu/rigeldev/kuberig/core/execution/GroupResourceGenerationFilter.kt
+++ kuberig-core/src/main/kotlin/eu/rigeldev/kuberig/core/execution/GroupResourceGenerationFilter.kt
@@ -1,16 +1,27 @@
 package eu.rigeldev.kuberig.core.execution
 
 import eu.rigeldev.kuberig.core.annotations.EnvResource
+import eu.rigeldev.kuberig.core.annotations.EnvResources
 import java.lang.reflect.Method
 
-class GroupResourceGenerationFilter(val groupNameMatcher: ResourceGroupNameMatcher) : ResourceGeneratorFilter {
+class GroupResourceGenerationFilter(private val groupNameMatcher: ResourceGroupNameMatcher) : ResourceGeneratorFilter {
 
     override fun shouldGenerate(method: Method): Boolean {
         val envResourceAnnotation = method.getDeclaredAnnotation(EnvResource::class.java)
-        val definedResourceGroupName : String? = if (envResourceAnnotation?.group == "") {
-            null
+        val envResourcesAnnotation = method.getDeclaredAnnotation(EnvResources::class.java)
+
+        val definedResourceGroupName : String? = if (envResourceAnnotation != null) {
+            if (envResourceAnnotation.group == "") {
+                null
+            } else {
+                envResourceAnnotation.group
+            }
         } else {
-            envResourceAnnotation?.group
+            if (envResourcesAnnotation.group == "") {
+                null
+            } else {
+                envResourcesAnnotation.group
+            }
         }
 
         return groupNameMatcher.matchResourceGroupName(definedResourceGroupName)
