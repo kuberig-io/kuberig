2d04a148e91db8ddb4c8a9d4ebac55fb4d4fbc1e
Tom Eyckmans <teyckmans@gmail.com>
2020-05-06 06:29:59 +0200

    update to Kotlin version 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-    val kotlinVersion by extra("1.3.50")
+    val kotlinVersion by extra("1.3.72")
 
     repositories {
         jcenter()
