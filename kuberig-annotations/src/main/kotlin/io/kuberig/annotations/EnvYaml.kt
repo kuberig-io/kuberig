d08e1372bebb4d4c99c4a366a88b6a928062f81f
Tom Eyckmans <teyckmans@gmail.com>
2021-01-11 05:57:22 +0100

    #36 YAML resource support
    - added EnvYaml annotation.

--- kuberig-annotations/src/main/kotlin/io/kuberig/annotations/EnvYaml.kt
+++ kuberig-annotations/src/main/kotlin/io/kuberig/annotations/EnvYaml.kt
@@ -1,12 +1,9 @@
 package io.kuberig.annotations
 
 /**
- * Marker annotation for methods that generate multiple resources.
- * The generated resources are published by calling one or multiple emit functions on io.kuberig.dsl.support.DslResourceEmitter.
+ * Marker annotation for methods that add resources from a YAML-file.
  */
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class EnvResources(
+annotation class EnvYaml(
     /**
      * Name of the group these resources belongs to.
      */
